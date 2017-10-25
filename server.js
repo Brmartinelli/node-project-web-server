6f2fa267ccccc3690804150c324df011791121e6
BrunoMartinelli <b.martinelli376@gmail.com>
2017-10-25 19:39:20 -0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const hbs = require ('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname +'/views/partials')
+
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next)=>{
+	var now = new Date().toString();
+	console.log (`${now}: ${req.method} ${req.url}`)
+
+
+	next();
+
+
+});
+
+
+hbs.registerHelper('getCurrentYear',() =>{
+	return new Date().getFullYear()
+
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+	return text.toUpperCase();
+
+
+});
+
+app.get('/',(req,res)=>{
+	
+//res.send('<h1>Hello Express!</h1>');
+
+
+	res.render('home.hbs',{
+		pageTitle:'Home Page',
+		welcomeMessage:'Welcome to my website',
+		currentYear: new Date().getFullYear()
+	});
+
+});
+
+
+app.get ('/about',(req, res)=>{
+
+//	res.send('About Page');
+
+	res.render('about.hbs', {
+		pageTitle:'About Page',
+		welcomeMessage:'Vai Curintia',
+		currentYear: new Date().getFullYear()});
+
+});
+
+// bad - send back json with errorMenssage
+
+app.get ('/bad',(req,res)=>{
+
+	res.send({
+		errosMessage: 'Unabe to handle request'
+	});
+
+});
+
+
+app.listen(3000,()=>{
+	console.log('Server is up on port 3000');
+
+
+});
+
+
+
+
